f0350f91e4cb530f529335ecf2b945ca2dcffafd
Pk <pk@thoughtworks.com>
2014-06-09 23:52:43 +0530

    [#1] Pk: remove dummy data

--- run.rb
+++ run.rb
@@ -9,18 +9,6 @@ require 'open-uri'
 
 XenoCanto.site= 'www.xeno-canto.org'
 
-def dummy_data
-  {'id' => '169792', 'gen' => 'Hydroprogne',
-   'sp' => 'caspia', 'ssp' => '', 'en' => 'Caspian Tern',
-   'rec' => 'pradnyavant mane', 'cnt' => 'India',
-   'loc' => 'Kalamb beach,Nala Sopara, Thane, Maharashtra', 'lat' => '19.4034',
-   'lng' => '72.7627', 'type' => 'call, flight call',
-   'file' => 'http://www.xeno-canto.org/169792/download',
-   'lic' => 'http://creativecommons.org/licenses/by-nc-sa/3.0/', 'url' => 'http://www.xeno-canto.org/169792'}
-end
-dummy_data.to_json
-# # Recording.new(dummy_data).download('')
-
 
 # Logging::LOG_LEVEL[:debug] = false
 recordings_page = RecordingsPage.new(1)
